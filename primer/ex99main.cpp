d111c0a56809fbcd28dd19416cce3197d50d93c7
sunyd <1404131532@qq.com>
2022-04-09 12:01:54 +0800

    done all

--- primer/ex99main.cpp
+++ primer/ex99main.cpp
@@ -1,50 +1,73 @@
-#include <iostream>
-#include <iterator>
-#include <vector>
-#include <cstring>
-#include <stdexcept>
-
-// current file include
-#include <tuple>
-
-// old
-#include "Sales_data.h"
-#include "Sales_item.h"
-#include "tool.h"
-#include <sstream>
 #include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-// define current function
+class Solution {
+public:
+    int numSubmatrixSumTarget(std::vector<std::vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        std::vector<std::vector<int>> sum(n + 1, std::vector<int>(m + 1));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                sum[i][j] =
+                    sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+        int ans = 0;
+        for (int top = 1; top <= n; top++) {
+            for (int bot = top; bot <= n; bot++) {
+                int cur = 0;
+                std::unordered_map<int, int> map;
+                for (int r = 1; r <= m; r++) {
+                    cur = sum[bot][r] - sum[top - 1][r];
+                    if (cur <= target) ans++;
+                    if (map.count(cur - target)) ans += map[cur - target];
+                    map[cur]++;
+                }
+            }
+        }
+        return ans;
+    }
+};
 
-// g14 tool.cpp Sales_data.cpp ex12.cpp ex12main.cpp -o main && ./main
 int main() {
+    int N, M, k;
+    ifstream fileIn("primer/ex99mainInput.txt");
+    string firstLine;
+    std::getline(fileIn, firstLine);
+    istringstream in_tmp(firstLine);
+    in_tmp >> N >> M >> k;
+    vector<vector<int>> martix;
+    while (N--) {
+        string line_tmp;
+        std::getline(fileIn, line_tmp);
+        istringstream row(line_tmp);
+        int m = M;
+        vector<int> line;
+        while (m--) {
+            int col;
+            row >> col;
+            line.push_back(col);
+        }
+        martix.push_back(line);
+    }
 
-    lnPrintln("\n习题 ------ ex15. ");
-
-
-
-
-
-    lnPrintln("\n习题 ------ ex15. ");
-
-
-
-
-
-    lnPrintln("\n习题 ------ ex15. ");
-
-
-
-
-
-    lnPrintln("\n习题 ------ ex15. ");
-
-
-
-
-    // lnPrintln("\n章节 ------------------ 10 ");
-
+    int ret = Solution().numSubmatrixSumTarget(martix, k);
+    cout << ret << " ";
     return 0;
 };
+
+/*
+
+3 3 0
+0 1 0
+1 1 1
+0 1 0
+
+
+*/
